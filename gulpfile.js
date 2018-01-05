4b7215c687d221aa634663d7fab59b9c0438fae0
Peter Bynum <>
2018-01-05 18:47:30 -0500

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,9 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
-        }
+        },
+        notify: false,
+        browser: "google chrome"
     });
 });
 
@@ -40,24 +42,24 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+   return gulp.src('_scss/main.scss')
+       .pipe(sass({
+           includePaths: ['scss'],
+           onError: browserSync.notify
+       }))
+       .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+       .pipe(gulp.dest('_site/assets/css/'))
+       .pipe(browserSync.reload({stream:true}))
+       .pipe(gulp.dest('assets/css/'));
 });
 
 /**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
+* Watch scss files for changes & recompile
+* Watch html/md files, run jekyll & reload BrowserSync
+*/
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+   gulp.watch('_scss/**', ['sass']);
+   gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*', 'assets/js/*.js'], ['jekyll-rebuild']);
 });
 
 /**
